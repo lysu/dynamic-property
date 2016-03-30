d08e89c511aa25b027b394a382c7b1a7223480c7
lysu <sulifx@gmail.com>
2016-03-31 00:22:32 +0800

    doc(dynamic): Add doc/example

--- viper.go
+++ viper.go
@@ -1,21 +1,3 @@
-// Dynamic-Property enhance viper with API return dynamic(or lazy) value.
-//
-// Viper is essentially repository for configurations
-// Get can retrieve any value given the key to use
-// Get has the behavior of returning the value associated with the first
-// place from where it is set. Viper will check in the following order:
-// override, flag, env, config file, key/value store, default
-// This package is base on viper, so we recommend to read viper document first at https://github.com/spf13/viper
-//
-// It's common for us to use viper with Etcd/consul or company's configuration management system.
-// And let properties have ability to modify dynamically during running...
-// Viper's origin API is get a `Value`, it's easy to lead us to save value into field or some cached-like hold
-// which are not easy to modify. So we extend them and return a function/a lazy value/ dynamic value.
-//
-// because Viper do memory cached internal, so there is no performance degrade to use dynamic value
-// Just replace viper.* with dynamic.* and take free to hold return value~
-//
-// At last, we recommend to see this example https://godoc.org/github.com/lysu/dynamic-property#example-DViper--DynamicProperty
 package dynamic
 
 import (
